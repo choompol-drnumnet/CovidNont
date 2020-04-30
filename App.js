5f4632681d510a5b590c99ca01f49dbb080cdeef
choompol-edipaorg <choompol@edipa.org>
2020-04-30 11:13:08 +0700

    non

--- App.js
+++ App.js
@@ -31,6 +31,17 @@ const newAppId = () => {
   return "CVN-"+tmst;
 }
 
+const initDb = async (tx) => {
+  const aid = newAppId();
+  console.log("INIT DB");
+  //let r1 = await tx.executeSql('drop table if exists tb1');
+  let r1 = await tx.executeSql('delete from tb1');
+  let r2 = await tx.executeSql('create table if not exists tb1(name varchar(80), phone varchar(15), fever numeric(6,2), staff char(1))');
+  let r3 = await tx.executeSql("insert into tb1(name,phone,fever,staff,appid) values('A','A','A','A')");
+  let r4 = await tx.executeSql("select name,phone,fever,staff from tb1");
+  console.log("INITING", r1, r2, r3,r4);
+};
+
 const App = () => {
   const [expTok, setExpTok] = useState('');
   const [notify, setNotify] = useState('');
@@ -48,17 +59,16 @@ const App = () => {
   const [wrongPhone, setWrongPhone] = useState(false);
   const [wrongFever, setWrongFever] = useState(false);
   const [wrongStaff, setWrongStaff] = useState(false);
-  const mgtop = 5;
-  const hgtxt = (hg*7/100);
-  const pos01 = (hg*38/100);
+
+  const [stType, setStType] = useState('');
 
   const regExpNotify = async () => {
     if (Constants.isDevice) {
-      const { status: state0 } = await 
+      const { status: state0 } = await
         Permissions.getAsync(Permissions.NOTIFICATIONS);
       let state1 = state0;
       if (state0 !== 'granted') {
-        const { state2 } = await 
+        const { state2 } = await
           Permissions.askAsync(Permissions.NOTIFICATIONS);
         state1 = state2;
       }
@@ -81,25 +91,23 @@ const App = () => {
       });
     }
   };
-
+  const mgtop = 5;
+  const hgtxt = (hg*7/100);
+  const pos01 = (hg*38/100);
   const expNotify = (n) => {
     Vibration.vibrate();
     setNotify(n.data.msg);
   };
-
   useEffect(() => {
     regExpNotify();
     const expNotSub = Notifications.addListener(expNotify);
   },[]);
-
+  //========== BARCODE CAMERA PREP
   useEffect(() => {
-    //========== BARCODE CAMERA PREP
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
-
-    //========== DATABASE PREP
     let s1 = "select name,phone,fever,staff,appid from "+tbnm;
     db.transaction(tx => {
       tx.executeSql(s1, [], (_, { rows }) => {
@@ -162,7 +170,7 @@ const App = () => {
         });
       });
      }
-  }, [pname,phone,pfever,pstaff]);
+  },[pname,phone,pfever,pstaff]);
 
   let fgSnd = 0;
 
@@ -172,7 +180,7 @@ const App = () => {
     fgSnd++;
     //console.log("SEND 2: ", fgSnd, data);
     let u0 = "https://ssjnonthaburi.moph.go.th/covidscan/mod_token/mod_server_temp.php";
-    
+
     let cd = data;
     let nm = pname;
     let ph = phone;
@@ -186,14 +194,14 @@ const App = () => {
 
     //console.log("APPID", appId);
     let url = u0 + "?COMPANYCODE=" + cd + "&NAME="+ nm
-       + "&PHONE="+ ph + "&TYPE=" + tp + "&TEMP="+ fe 
+       + "&PHONE="+ ph + "&TYPE=" + tp + "&TEMP="+ fe
        + "&APPID="+appId+ "&TOKEN="+ expTok;
 
     console.log("URL:",url);
-    fetch(url, { method: 'GET', }) 
+    fetch(url, { method: 'GET', })
     .then((resp) => {
       fgSnd = 0;
-      return resp.text(); 
+      return resp.text();
     })
     .then((text) => {
       fgSnd = 0;
@@ -208,7 +216,7 @@ const App = () => {
 
   return (
 
-    <ImageBackground source={require("./assets/background.jpg")}
+    <ImageBackground source={require("./assets/background2.png")}
       style={{
         flex: 1,
         flexDirection: 'column',
@@ -216,14 +224,18 @@ const App = () => {
       }}>
       {mode===1 &&
 
-      <KeyboardAvoidingView 
+      <KeyboardAvoidingView
         style={{width: '100%', alignItems: 'center'}}
         behavior="padding" enabled
       >
+      <View style={{margin:10,top:210}}>
+        <Text style={{fontSize:12,color:'#fff'}}>คำแนะนำการใช้งาน</Text>
+        <Text style={{fontSize:12,color:'#fff'}}>เพื่อให้ท่านปลอดภัยและสามารถนำข้อมูลไปใช้เพื่อดูแลท่านในกรณีเกิดการ แพร่ระบาดในสถานที่ที่ท่านเคยไปใช้บริการและจากการสัมผัสผู้ป่วยยืนยันโรคโควิด-19  จึงขอความร่วมมือ ดังนี้</Text>
+        <Text style={{fontSize:12,color:'#fff'}}>1.ขอให้ท่านบันทึกชื่อ นามสกุล หมายเลขโทรศัพท์ที่ติดต่อได้ เพียงครั้งเดียว</Text>
+        <Text style={{fontSize:12,color:'#fff'}}>2.ให้ระบุว่า เป็นผู้ให้บริการหรือผู้รับบริการ และต้องบันทึกว่ามีอาการไข้หรือปกติ</Text>
+        <Text style={{fontSize:12,color:'#fff'}}>3.กดที่ปุ่ม "สแกน QR code" กับ  ร้าน หน่วยบริการ ฯลฯ ที่ได้ขึ้นทะเบียนมี QR code เฝ้าระวังโควิด-19 จังหวัดนนทบุรี</Text>
+      </View>
       <View style={{
-        marginTop: pos01,
-        height: '57%',
-        marginHorizontal: (hg*2/100),
         flexDirection: 'column',
         alignItems: 'flex-start',
        }}>
@@ -231,73 +243,40 @@ const App = () => {
         <View style={{
           width: (wd*60/100),
           height: hgtxt,
-          marginTop: 20,
-          borderWidth: 1,
+          marginTop:230,
           borderRadius: 10,
-          borderColor: 'white',
-          backgroundColor: 'lightblue',
           flexDirection: 'row',
           alignItems: 'center',
           justifyContent: 'center',
           alignSelf: 'center',
           }}>
-          <TouchableOpacity onPress={()=>{ 
+          <TouchableOpacity  style={styles.FacebookStyle} activeOpacity={0.5} onPress={()=>{
               // ============== PUSH SCAN
               fgSnd = 0;
               setMode(3);
               setTimeout(()=>setMode(1), 10000);
-            }}>
-            <Text style={{
-              paddingVertical: 3,
-            }}>แสกนคิวอาร์โค้ด</Text>
+            }}
+
+            >
+            <Image
+             source={require('./assets/1.png')}
+             style={styles.ImageIconStyle}
+             />
+            <Text style={styles.TextStyle}> สแกน QR CODE</Text>
           </TouchableOpacity>
         </View>
 
-        <View style={{
-          marginTop: 20,
-          marginLeft: 20,
-          borderWidth: 1,
-          borderRadius: 10,
-          borderColor: 'white',
-          backgroundColor: 'lightblue',
-          flexDirection: 'row',
-          }}>
-          <Text style={{
-              alignSelf: 'center',
-              padding: 5,
-            }}>ชื่อ</Text>
-          <TextInput style={{
-              padding: 5,
-              marginVertical: 2,
-              marginHorizontal: 5,
-              width: '80%',
-              backgroundColor: 'white',
-            }} maxLength={50}
+        <View>
+          <Text style={{fontSize:15,color:'#fff'}}>ชื่อ</Text>
+          <TextInput style={{top:10,height: 40, textAlign: 'center',  color:'#000', width:350, borderColor: 'gray', borderRadius: 10, borderWidth: 1,backgroundColor:'#FFFFFF' }} maxLength={50}
             autoCapitalize="none" autoCorrect={false}
             onChangeText={(v)=>{ setPname(v); }} value={pname}
             />
         </View>
 
-        <View style={{
-          marginTop: mgtop,
-          marginLeft: 20,
-          borderWidth: 1,
-          borderRadius: 10,
-          borderColor: 'white',
-          backgroundColor: 'lightblue',
-          flexDirection: 'row',
-          }}>
-          <Text style={{
-              alignSelf: 'center',
-              padding: 5,
-            }}>โทร</Text>
-          <TextInput style={{
-              padding: 5,
-              marginVertical: 2,
-              marginHorizontal: 5,
-              width: '60%',
-              backgroundColor: 'white',
-            }} maxLength={50}
+        <View>
+          <Text style={{top:15,fontSize:15,color:'#fff'}}>เบอร์โทร</Text>
+          <TextInput style={{top:15,height: 40, textAlign: 'center',  color:'#000', width:350, borderColor: 'gray', borderRadius: 10, borderWidth: 1,backgroundColor:'#FFFFFF' }} maxLength={50}
             autoCapitalize="none" autoCorrect={false}
             onChangeText={(v)=>{ setPhone(v); }} value={phone}
             />
@@ -306,43 +285,45 @@ const App = () => {
         <View style={{
           height: hgtxt,
           marginTop: mgtop,
-          marginLeft: 20,
-          borderWidth: 1,
           borderRadius: 10,
-          borderColor: 'white',
-          backgroundColor: 'lightblue',
           flexDirection: 'row',
           alignItems: 'center',
           justifyContent: 'center',
           }}>
-          <TouchableOpacity onPress={()=>{ 
+          <TouchableOpacity onPress={()=>{
               // ============== PUSH SELLER
               setPstaff('1');
               //console.log("SELLER");
             }}>
             <Text style={{
+              top:10,
+              fontSize:20,
+              borderRadius: 10,
               marginVertical: 2,
               marginHorizontal: 5,
-              paddingHorizontal: 20,
-              paddingVertical: 2,
+              paddingHorizontal: 30,
+              paddingVertical: 4,
               color: (pstaff=='1'? 'blue':'black'),
-              backgroundColor: (pstaff=='1'? 'white':'lightgrey'),
+              backgroundColor: (pstaff=='1'? 'white':'#fff'),
               borderColor: (pstaff=='1'? 'blue':'white'),
               borderWidth: 1,
             }}>ผู้ให้บริการ</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=>{ 
+          <TouchableOpacity onPress={()=>{
               // ============== PUSH BUYER
               setPstaff('2');
               console.log("BUYER");
             }}>
             <Text style={{
+              top:10,
+              fontSize:20,
+              borderRadius: 10,
               marginVertical: 2,
               marginHorizontal: 5,
-              paddingHorizontal: 20,
-              paddingVertical: 2,
+              paddingHorizontal: 45,
+              paddingVertical: 4,
               color: (pstaff=='2'? 'blue':'black'),
-              backgroundColor: (pstaff=='2'? 'white':'lightgrey'),
+              backgroundColor: (pstaff=='2'? 'white':'#fff'),
               borderColor: (pstaff=='2'? 'blue':'white'),
             }}>ผู้ใช้บริการ</Text>
           </TouchableOpacity>
@@ -351,41 +332,41 @@ const App = () => {
         <View style={{
           height: hgtxt,
           marginTop: mgtop,
-          marginLeft: 20,
-          borderWidth: 1,
           borderRadius: 10,
-          borderColor: 'white',
-          backgroundColor: 'lightblue',
           flexDirection: 'row',
           alignItems: 'center',
           justifyContent: 'center',
           }}>
-          <TouchableOpacity onPress={()=>{ 
+          <TouchableOpacity style={{}} onPress={()=>{
               // ============== PUSH SELLER
               setPfever('1');
             }}>
             <Text style={{
+              fontSize:20,
+              borderRadius: 10,
               marginVertical: 2,
               marginHorizontal: 5,
-              paddingHorizontal: 20,
-              paddingVertical: 2,
+              paddingHorizontal: 55,
+              paddingVertical: 4,
               color: (pfever=='1'? 'blue':'black'),
-              backgroundColor: (pfever=='1'? 'white':'lightgrey'),
+              backgroundColor: (pfever=='1'? 'white':'#fff'),
               borderColor: (pfever=='1'? 'blue':'white'),
               borderWidth: 1,
             }}>มีไข้</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=>{ 
+          <TouchableOpacity onPress={()=>{
               // ============== PUSH BUYER
               setPfever('2');
             }}>
             <Text style={{
+              fontSize:20,
+              borderRadius: 10,
               marginVertical: 2,
               marginHorizontal: 5,
-              paddingHorizontal: 20,
-              paddingVertical: 2,
+              paddingHorizontal: 60,
+              paddingVertical: 5,
               color: (pfever=='2'? 'blue':'black'),
-              backgroundColor: (pfever=='2'? 'white':'lightgrey'),
+              backgroundColor: (pfever=='2'? 'white':'#fff'),
               borderColor: (pfever=='2'? 'blue':'white'),
             }}>ไม่มีไข้</Text>
           </TouchableOpacity>
@@ -415,7 +396,7 @@ const App = () => {
       </View>
       </KeyboardAvoidingView>
       }
-      {mode==2 && 
+      {mode==2 &&
       <View style={{
         marginTop: pos01,
         height: '57%',
@@ -438,7 +419,7 @@ const App = () => {
           alignSelf: 'center',
           color: ((!wrongName && !wrongPhone && !wrongFever && !wrongStaff)? 'black' : 'gray'),
           }}>
-          <TouchableOpacity onPress={()=>{ 
+          <TouchableOpacity onPress={()=>{
               // ============== PUSH SCAN
               if(!wrongName && !wrongPhone && !wrongFever && !wrongStaff) {
                 fgSnd = 0;
@@ -462,7 +443,7 @@ const App = () => {
           }}>
           <BarCodeScanner
             onBarCodeScanned = {handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject} /> 
+            style={StyleSheet.absoluteFillObject} />
           <View style={{
             width: (wd*60/100),
             height: hgtxt,
@@ -484,10 +465,138 @@ const App = () => {
             </TouchableOpacity>
           </View>
         </View>
+
       }
+      <View style={styles.text_bottom_container}>
+        <Text style={styles.text_bottom_style}>หมายเหตุ</Text>
+        <Text style={styles.text_bottom_style}>1.ข้อมูลนี้ใช้เฉพาะภารกิจการเฝ้าระวัง ป้องกันโรคโควิด-19จังหวัดนนทบุรีเท่านั้น หากเสร็จสิ้นภาระกิจฯข้อมูลท่านจะถูกลบทิ้ง</Text>
+        <Text style={styles.text_bottom_style}>2.ข้อมูลของท่านอยู่ในระบบและการดูแลโดยสำนักงานสาธารณสุขจังหวัดนนทบุรี โทร 061-3945402-3</Text>
+      </View>
     </ImageBackground>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  ImageIconStyle: {
+   left:10,
+   height: 50,
+   width: 50,
+   resizeMode : 'stretch',
 
+},
+
+TextStyle :{
+  color: "#fff",
+  left:15,
+  marginBottom : 4,
+  marginRight :30,
+
+},
+
+SeparatorLine :{
+
+backgroundColor : '#fff',
+width: 1,
+height: 40
+
+},
+  MainContainer: {
+  top:10,
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   margin: 10
+ },
+ FacebookStyle: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  backgroundColor: '#485a96',
+  borderWidth: .5,
+  borderColor: '#fff',
+  height:60,
+  borderRadius: 5 ,
+  margin: 5,
+
+},
+  text_top_container:{
+    top:100
+  },
+  text_bottom_container:{
+    left:10,
+    top:30
+  },
+  text_bottom_style:{
+      color:'#fff'
+  },
+  text_tops:{
+    marginHorizontal:10,
+    left:10,
+    margin:4,
+    fontSize:12,
+    color:'#fff'
+  },
+  text_top:{
+      margin:4,
+      fontSize:12,
+      color:'#fff'
+  },
+  buttonContainer: {
+    top:35,
+    left:30,
+    fontSize:20,
+    width:350,
+    flexDirection: "row",
+    marginBottom: 20,
+    justifyContent: "center",
+    borderRadius: 15
+  },
+  button_scan:{
+    top:110,
+    fontSize:20
+  },
+  container: {
+    top:25,
+    flex: 1,
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  checkbox: {
+    alignSelf: "center",
+  },
+  checkboxContainers:{
+    top:45,
+    left:30,
+    backgroundColor:'#fff',
+    width:350,
+    flexDirection: "row",
+    marginBottom: 20,
+    justifyContent: "center",
+    borderRadius: 15
+  },
+  checkboxContainer: {
+   top:45,
+   left:30,
+   backgroundColor:'#fff',
+   width:350,
+   flexDirection: "row",
+   marginBottom: 20,
+   justifyContent: "center",
+   borderRadius: 15
+ },
+ label: {
+    fontSize:20,
+    margin: 8,
+    color:'#000'
+  }
+});
+
+export default App;
